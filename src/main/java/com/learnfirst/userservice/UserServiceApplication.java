8a0dca63476bb2851809498354d3a77f9f923174
jgantayat <gantayatjanmajaya@gmail.com>
2026-03-03 12:20:15 +0530

    Add configuration files and implement user service functionality

--- src/main/java/com/learnfirst/userservice/UserServiceApplication.java
+++ src/main/java/com/learnfirst/userservice/UserServiceApplication.java
@@ -1,14 +1,22 @@
 package com.learnfirst.userservice;
 
+import com.learnfirst.userservice.service.DataService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+@RequiredArgsConstructor
+public class UserServiceApplication  implements CommandLineRunner {
 
+//	private final DataService dataService;
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
-
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("This data is :"+ dataService.getData());
+	}
 }
